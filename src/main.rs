6291a788bf8b88877caee93e02dacbbfb2ee0d32
Adornadowilliam2 <145187649+Adornadowilliam2@users.noreply.github.com>
2024-06-23 08:47:47 -0700

    update a bit like add input and output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io; 
+
 fn main() {
     println!("Hello, world!");
+
+   
+    println!("Please enter something:");
+
+
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+      
+            println!("You entered: {}", input);
+        }
+        Err(error) => {
+            eprintln!("Error reading input: {}", error);
+        }
+    }
 }
