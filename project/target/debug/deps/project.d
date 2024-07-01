cebd7903aa6ccafba11e4dda89a31699f452490c
Adornadowilliam2 <145187649+Adornadowilliam2@users.noreply.github.com>
2024-07-01 20:20:00 +0800

    Add?

---
+++ project/target/debug/deps/project.d
@@ -0,0 +1,5 @@
+C:\Users\Fe Adornado\Downloads\Rust\simple_calculator\project\target\debug\deps\project.exe: src/main.rs
+
+C:\Users\Fe Adornado\Downloads\Rust\simple_calculator\project\target\debug\deps\project.d: src/main.rs
+
+src/main.rs:
---
+++ project/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\Fe\ Adornado\Downloads\Rust\simple_calculator\project\target\debug\project.exe: C:\Users\Fe\ Adornado\Downloads\Rust\simple_calculator\project\src\main.rs
